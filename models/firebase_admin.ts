729bef161832d9e2d6ecdda2a7257dfe282b016d
junyoung <jadw9715@gmail.com>
2022-10-22 23:00:04 +0900

    feat 공부 시작

---
+++ models/firebase_admin.ts
@@ -0,0 +1,57 @@
+import * as admin from 'firebase-admin';
+
+interface Config {
+  credential: {
+    privateKey: string;
+    clientEmail: string;
+    projectId: string;
+  };
+}
+
+export default class FirebaseAdmin {
+  public static instance: FirebaseAdmin;
+
+  private init = false;
+
+  public static getInstance(): FirebaseAdmin {
+    if (FirebaseAdmin.instance === undefined || FirebaseAdmin.instance === null) {
+      //초기화
+      FirebaseAdmin.instance = new FirebaseAdmin();
+      // TODO: 환경을 초기화한다
+    }
+    return FirebaseAdmin.instance;
+  }
+
+  private bootstrap(): void {
+    const haveApp = admin.apps.length !== 0;
+    if (haveApp) {
+      this.init = true;
+      return;
+    }
+
+    const config: Config = {
+      credential: {
+        projectId: process.env.projectId || '',
+        clientEmail: process.env.clientEmail || '',
+        privateKey: (process.env.privateKey || '').replace(/\\n/g, '\n'),
+      },
+    };
+    admin.initializeApp({ credential: admin.credential.cert(config.credential) });
+    console.info('bootstrap firebase admin');
+  }
+
+  /** firestore를 반환 */
+  public get Firebase(): FirebaseFirestore.Firestore {
+    if (this.init === false) {
+      this.bootstrap();
+    }
+    return admin.firestore();
+  }
+
+  public get Auth(): admin.auth.Auth {
+    if (this.init === false) {
+      this.bootstrap();
+    }
+    return admin.auth();
+  }
+}
