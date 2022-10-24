233f731b54fcf4bdf8ca590e17cc4e932ca65bfc
junyoung <jadw9715@gmail.com>
2022-10-24 23:12:40 +0900

    브라우저에 Firebase SDK 인증을 위한 작업

---
+++ models/firebase_client.ts
@@ -0,0 +1,38 @@
+import { getApps, initializeApp } from 'firebase/app';
+import { Auth, getAuth } from 'firebase/auth';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+
+const FirebaseCredential = {
+  apiKey: publicRuntimeConfig.apiKey,
+  authDomain: publicRuntimeConfig.authDomain,
+  projectId: publicRuntimeConfig.projectId,
+};
+
+export default class FirebaseClient {
+  private static instance: FirebaseClient;
+
+  private auth: Auth;
+
+  public constructor() {
+    const apps = getApps();
+    if (apps.length === 0) {
+      console.info('firebase client init start');
+      initializeApp(FirebaseCredential);
+    }
+    this.auth = getAuth();
+    console.info('firebase auth');
+  }
+
+  public static getInstance(): FirebaseClient {
+    if (FirebaseClient.instance === undefined || FirebaseClient.instance === null) {
+      FirebaseClient.instance = new FirebaseClient();
+    }
+    return FirebaseClient.instance;
+  }
+
+  public get Auth(): Auth {
+    return this.auth;
+  }
+}
