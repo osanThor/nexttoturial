0c7abcae7fe241adab772a4e0a7ec5bc1c768b04
junyoung <jadw9715@gmail.com>
2022-10-25 23:48:10 +0900

    feat. main GNB

--- components/service_layout.tsx
+++ components/service_layout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import GNB from './GNB';
 
 interface Props {
   title: string;
@@ -11,6 +12,7 @@ export const ServiceLayout = function ({ title = 'blah x2', children }: Props) {
       <Head>
         <title>{title}</title>
       </Head>
+      <GNB />
       {children}
     </div>
   );
