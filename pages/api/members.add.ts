e4dac57d0670f7b493363dcb91a4fb1d2422636a
junyoung <jadw9715@gmail.com>
2022-12-31 02:37:02 +0900

    코드 간소화 에러 이벤트 공통 모듈

--- pages/api/members.add.ts
+++ pages/api/members.add.ts
@@ -2,17 +2,18 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
 import MemberCtrl from '@/controllers/members.ctrl';
+import handleError from '@/controllers/error/handle_error';
+import checkSupportMethod from '@/controllers/error/check_support_method';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { method } = req;
-  const supporMethod = ['POST'];
+  const supportMethod = ['POST'];
   try {
-    if (supporMethod.indexOf(method!) === -1) {
-      //에러 반환
-    }
+    checkSupportMethod(supportMethod, method);
     await MemberCtrl.add(req, res);
   } catch (err) {
     console.error(err);
     //에러 처리
+    handleError(err, res);
   }
 }
