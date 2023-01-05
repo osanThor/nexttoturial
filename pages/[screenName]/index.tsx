9ac1fa46a15f458242c8927ac46c15d43b54d61a
junyoung <jadw9715@gmail.com>
2023-01-05 22:48:48 +0900

    style. user info layout

--- pages/[screenName]/index.tsx
+++ pages/[screenName]/index.tsx
@@ -14,10 +14,12 @@ const UserHomePage: NextPage = function () {
     <ServiceLayout title="user home" minH="100vh" backgroundColor="gray.50">
       <Box maxW="md" mx="auto" pt="6">
         <Box borderWidth="1px" borderRadius="lg" overflow="hidden" mb="2" bg="white">
-          <Avatar size="lg" src={userInfo.photoURL} mr="2" />
-          <Flex direction="column" justify="center">
-            <Text fontSize="md">{userInfo.displayName}</Text>
-            <Text fontSize="xs">{userInfo.email}</Text>
+          <Flex p="6">
+            <Avatar size="lg" src={userInfo.photoURL} mr="2" />
+            <Flex direction="column" justify="center">
+              <Text fontSize="md">{userInfo.displayName}</Text>
+              <Text fontSize="xs">{userInfo.email}</Text>
+            </Flex>
           </Flex>
         </Box>
       </Box>
