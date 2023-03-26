2de950525907544158b3631c6f7ca73bb835090e
junyoung <jadw9715@gmail.com>
2023-03-26 23:23:43 +0900

    feat: thumbnail 이미지 설정

---
+++ pages/api/thumbnail.ts
@@ -0,0 +1,43 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import Chromium from 'chrome-aws-lambda';
+import playwright from 'playwright-core';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const localChromePath = process.env.NODE_ENV !== 'development' ? '' : process.env.LOCAL_CHROME_PATH ?? '';
+  if (process.env.NODE_ENV !== 'development') {
+    const protocol = process.env.PROTOCOL || 'http';
+    const host = process.env.HOST || 'localhost';
+    const port = process.env.PORT || '3000';
+    const baseUrl = `${protocol}://${host}:${port}`;
+    await Chromium.font(`${baseUrl}/Pretendard-Regular.ttf`);
+  }
+
+  const browser = await playwright.chromium.launch({
+    args: Chromium.args,
+    executablePath: process.env.NODE_ENV !== 'development' ? await Chromium.executablePath : localChromePath,
+    headless: process.env.NODE_ENV !== 'development' ? Chromium.headless : true,
+  });
+
+  const page = await browser.newPage({
+    viewport: {
+      width: 1200,
+      height: 675,
+    },
+  });
+
+  const url = req.query.url as string;
+
+  await page.goto(url);
+
+  const data = await page.screenshot({
+    type: 'jpeg',
+  });
+
+  await browser.close();
+
+  res.setHeader('Cache-Control', 's-maxage=31536000, public');
+  res.setHeader('Content-Type', 'image/jpeg');
+  res.end(data);
+}
