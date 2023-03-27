88ca0e0534761f6ec0797c926aa3a6cbf8d1ece0
junyoung <jadw9715@gmail.com>
2023-03-27 22:43:21 +0900

    fix: build 수정

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 module.exports = {
+  api: {
+    bodyParser: {
+      sizeLimit: '500kb',
+    },
+  },
   reactStrictMode: true,
   publicRuntimeConfig: {
     apiKey: process.env.publicApiKey || '',
     authDomain: process.env.FIREBASE_AUTH_HOST || '',
     projectId: process.env.projectId || '',
-  },
-  api: {
-    responseLimit: false,
   },
 };
